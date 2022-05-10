49e1f75c038235a2ce26bbb2bc34a31e15531df2
Dan Whyte <danwhyte1986@gmail.com>
2022-05-10 16:02:52 +0100

    add inputs

--- rockpaper/script.js
+++ rockpaper/script.js
@@ -1,1 +1,22 @@
-console.log("Daniel!");
+
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+   let answer =  window.prompt("Enter Rock, Paper or Scissors");
+    
+    console.log("Player says " + answer);
+
+    console.log("Computer says " + computerSelection);
+    
+  }
+  
+  const myArray = ["Rock", "Paper", "Scissors"];
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
+
+  
+
+function computerPlay() {
+  return myArray[~~(Math.random() * myArray.length)];
+}
