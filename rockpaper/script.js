d464cb3a8a649a00e2de460af1f1469757bab900
Dan Whyte <danwhyte1986@gmail.com>
2022-05-15 18:58:30 +0100

    Display player and computer choice

--- rockpaper/script.js
+++ rockpaper/script.js
@@ -3,14 +3,23 @@
 function playRound(playerSelection, computerSelection) {
     // your code here!
    let answer =  window.prompt("Enter Rock, Paper or Scissors");
+   let theAnswer = answer.toLowerCase();
     
-    console.log("Player says " + answer);
-
-    console.log("Computer says " + computerSelection);
+    
     // inputs
+    if ((theAnswer == "rock") || (theAnswer == "paper") || (theAnswer == "scissors" )){
+      playerSelection = theAnswer;  
+        console.log("You chose " + playerSelection);
+        
+      } else { console.log("Please make a valid selection!"); }
+  
+      console.log("Computer says " + computerSelection);
+
+
+    
   }
   
-  const myArray = ["Rock", "Paper", "Scissors"];
+  const myArray = ["rock", "paper", "scissors"];
   const playerSelection = "rock";
   const computerSelection = computerPlay();
   console.log(playRound(playerSelection, computerSelection));
