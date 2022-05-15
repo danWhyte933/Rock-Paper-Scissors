37c0ee1162c1d1e2e9945f8db85940b0da719f5d
Dan Whyte <danwhyte1986@gmail.com>
2022-05-15 19:14:35 +0100

    Add win or lose

--- rockpaper/script.js
+++ rockpaper/script.js
@@ -11,11 +11,15 @@ function playRound(playerSelection, computerSelection) {
       playerSelection = theAnswer;  
         console.log("You chose " + playerSelection);
         
-      } else { console.log("Please make a valid selection!"); }
+      } else { console.log("Please make a valid selection!");  }
   
       console.log("Computer says " + computerSelection);
 
-
+    if (computerSelection == playerSelection) { console.log("A Draw"); }
+    else if ((computerSelection == "rock" && playerSelection == "scissors") ||
+    (computerSelection == "scissors" && playerSelection == "paper") ||
+    (computerSelection == "paper" && playerSelection == "rock"))
+    { console.log("You lose"); } else {console.log("You Win!"); }
     
   }
   
